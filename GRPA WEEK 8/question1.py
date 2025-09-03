5b994bd7bf3954e1bda9cfa874f882d391e78eaf
Saurav Kumar <davidsauravyadav@gmail.com>
2025-09-03 09:11:47 +0530

    Update question1.py

--- GRPA WEEK 8/question1.py
+++ GRPA WEEK 8/question1.py
@@ -1,1 +1,24 @@
+def get_freq(filename):
+    """
+    Extract frequency information from the file
 
+    Argument:
+    filename: string, path to file
+
+    Return:
+    result: dictionary; keys are strings, values are integers
+    """
+    try:
+        f = open(filename, 'r')
+    except Exception as e:
+        return
+
+    words = f.readlines()
+    d = {}
+    for word in words:
+        s_word = word.strip()
+        if s_word not in d:
+            d[s_word] = 1
+        else:
+            d[s_word] += 1
+    return d
